bd31741a67ac0a39defa64af951da54c5f2c19a6
Dima Tsurkan <dtsurkan@monsterlg.com>
2022-06-14 18:29:06 +0300

    Commit 2022

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,3 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/brands';
 import './index.scss';
-
-class App {
-    constructor() {
-
-    }
-}
-
-(() => {
-    new App();
-})();
